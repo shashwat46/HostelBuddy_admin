c7760793565a49ed3d8f529433acbf68899f498b
shashwatsingh <shashwat.singh0406@gmail.com>
2024-03-19 18:48:48 +0530

    initial commit

---
+++ HostelBuddy_admin/HostelBuddy_admin/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  HostelBuddy_admin
+//
+//  Created by Shashwat Singh on 19/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HostelBuddy_admin/HostelBuddy_admin/HostelBuddy_adminApp.swift
@@ -0,0 +1,17 @@
+//
+//  HostelBuddy_adminApp.swift
+//  HostelBuddy_admin
+//
+//  Created by Shashwat Singh on 19/3/24.
+//
+
+import SwiftUI
+
+@main
+struct HostelBuddy_adminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
